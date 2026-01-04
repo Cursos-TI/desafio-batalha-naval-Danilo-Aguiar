9497938b9b3318f1ce5303a2e3f3866da77a21ec
Danilo-Aguiar <danilo2021.aguiar@gmail.com>
2026-01-04 20:33:57 -0300

    Update batalhaNaval.c
    Foram criado 3 matrizes: Cruz, Octaedro e Cone. Ocupando espaços no tabuleiro representando figuras. Foi utilizado constantes, loops aninhados e condicionais para implementação do código.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,155 +1,128 @@
 #include <stdio.h>
 
-#define TAM_TABULEIRO 10
-#define TAM_NAVIO 3
+#define COLUNA 10
+#define LINHA 10
+#define HAB 5
+
 #define AGUA 0
 #define NAVIO 3
-
+#define HABILIDADE 3   
 int main() {
 
-    int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO];
+    int tabuleiro[COLUNA][LINHA];
+    int cone[HAB][HAB];
+    int cruz[HAB][HAB];
+    int octaedro[HAB][HAB];
 
-    // Inicializa o tabuleiro com água
-    for (int i = 0; i < TAM_TABULEIRO; i++) {
-        for (int j = 0; j < TAM_TABULEIRO; j++) {
+    int centro = HAB / 2;
+
+    // Inicializa tabuleiro
+    for (int i = 0; i < COLUNA; i++)
+        for (int j = 0; j < LINHA; j++)
             tabuleiro[i][j] = AGUA;
+
+    // Navio exemplo
+    tabuleiro[4][4] = NAVIO;
+    tabuleiro[4][5] = NAVIO;
+    tabuleiro[4][6] = NAVIO;
+
+    // ================= CONE =================
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+
+            cone[i][j] = 0;
+            if (i < centro) continue;
+
+            int alcance = i - centro;
+            if (j < centro - alcance) continue;
+            if (j > centro + alcance) continue;
+
+            cone[i][j] = 3;
         }
     }
 
-    // ============================
-    // Coordenadas iniciais dos navios
-    // ============================
-
-    // Navio horizontal
-    int linhaH = 3;      // Linha 3
-    char colunaH = 'B';  // Coluna B
-
-    // Navio vertical
-    int linhaV = 6;      // Linha 6
-    char colunaV = 'H';  // Coluna H
-
-    // Navio diagonal ↘
-    int linhaD1 = 1;     // Linha 1
-    char colunaD1 = 'A'; // Coluna A
-
-    // Navio diagonal ↙
-    int linhaD2 = 4;     // Linha 4
-    char colunaD2 = 'J'; // Coluna J
-
-    // Conversão para índices do array
-    int linhaHIdx = linhaH - 1;
-    int colunaHIdx = colunaH - 'A';
-
-    int linhaVIdx = linhaV - 1;
-    int colunaVIdx = colunaV - 'A';
-
-    int linhaD1Idx = linhaD1 - 1;
-    int colunaD1Idx = colunaD1 - 'A';
-
-    int linhaD2Idx = linhaD2 - 1;
-    int colunaD2Idx = colunaD2 - 'A';
-
-    // ============================
-    // Navio horizontal
-    // ============================
-    if (colunaHIdx + TAM_NAVIO <= TAM_TABULEIRO) {
-        int livre = 1;
-
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaHIdx][colunaHIdx + i] != AGUA) {
-                livre = 0;
-                break;
-            }
-        }
-
-        if (livre) {
-            for (int i = 0; i < TAM_NAVIO; i++) {
-                tabuleiro[linhaHIdx][colunaHIdx + i] = NAVIO;
-            }
+    // ================= CRUZ =================
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
+            cruz[i][j] = 0;
+            if (i == centro) cruz[i][j] = 3;
+            if (j == centro) cruz[i][j] = 3;
         }
     }
 
-    // ============================
-    // Navio vertical
-    // ============================
-    if (linhaVIdx + TAM_NAVIO <= TAM_TABULEIRO) {
-        int livre = 1;
+    // ================= OCTAEDRO =================
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
 
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaVIdx + i][colunaVIdx] != AGUA) {
-                livre = 0;
-                break;
-            }
-        }
+            int l = (i > centro) ? i - centro : centro - i;
+            int c = (j > centro) ? j - centro : centro - j;
 
-        if (livre) {
-            for (int i = 0; i < TAM_NAVIO; i++) {
-                tabuleiro[linhaVIdx + i][colunaVIdx] = NAVIO;
-            }
+            octaedro[i][j] = 0;
+            if (l + c <= centro)
+                octaedro[i][j] = 3;
         }
     }
 
-    // ============================
-    // Navio diagonal ↘
-    // ============================
-    if (linhaD1Idx + TAM_NAVIO <= TAM_TABULEIRO &&
-        colunaD1Idx + TAM_NAVIO <= TAM_TABULEIRO) {
+    // Origens
+    int oConeL = 2, oConeC = 2;
+    int oCruzL = 6, oCruzC = 2;
+    int oOctL  = 6, oOctC  = 7;
 
-        int livre = 1;
+    // Sobreposição das habilidades
+    for (int i = 0; i < HAB; i++) {
+        for (int j = 0; j < HAB; j++) {
 
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaD1Idx + i][colunaD1Idx + i] != AGUA) {
-                livre = 0;
-                break;
+            if (cone[i][j] == 3) {
+                int l = oConeL + i - centro;
+                int c = oConeC + j - centro;
+
+                if (l < 0) continue;
+                if (l >= COLUNA) continue;
+                if (c < 0) continue;
+                if (c >= LINHA) continue;
+
+                if (tabuleiro[l][c] == AGUA)
+                    tabuleiro[l][c] = HABILIDADE;
             }
-        }
 
-        if (livre) {
-            for (int i = 0; i < TAM_NAVIO; i++) {
-                tabuleiro[linhaD1Idx + i][colunaD1Idx + i] = NAVIO;
+            if (cruz[i][j] == 3) {
+                int l = oCruzL + i - centro;
+                int c = oCruzC + j - centro;
+
+                if (l < 0) continue;
+                if (l >= COLUNA) continue;
+                if (c < 0) continue;
+                if (c >= LINHA) continue;
+
+                if (tabuleiro[l][c] == AGUA)
+                    tabuleiro[l][c] = HABILIDADE;
+            }
+
+            if (octaedro[i][j] == 3) {
+                int l = oOctL + i - centro;
+                int c = oOctC + j - centro;
+
+                if (l < 0) continue;
+                if (l >= COLUNA) continue;
+                if (c < 0) continue;
+                if (c >= LINHA) continue;
+
+                if (tabuleiro[l][c] == AGUA)
+                    tabuleiro[l][c] = HABILIDADE;
             }
         }
     }
 
-    // ============================
-    // Navio diagonal ↙
-    // ============================
-    if (linhaD2Idx + TAM_NAVIO <= TAM_TABULEIRO &&
-        colunaD2Idx - (TAM_NAVIO - 1) >= 0) {
+    // Exibição final
+    printf("\n0 = Agua | 3 = Navio (inclui habilidade)\n\n");
 
-        int livre = 1;
-
-        for (int i = 0; i < TAM_NAVIO; i++) {
-            if (tabuleiro[linhaD2Idx + i][colunaD2Idx - i] != AGUA) {
-                livre = 0;
-                break;
-            }
-        }
-
-        if (livre) {
-            for (int i = 0; i < TAM_NAVIO; i++) {
-                tabuleiro[linhaD2Idx + i][colunaD2Idx - i] = NAVIO;
-            }
-        }
-    }
-
-    // ============================
-    // Exibição do tabuleiro com coordenadas
-    // ============================
-    printf("    ");
-    for (char c = 'A'; c < 'A' + TAM_TABULEIRO; c++) {
-        printf("%c ", c);
-    }
-    printf("\n");
-
-    for (int i = 0; i < TAM_TABULEIRO; i++) {
-        printf("%2d  ", i + 1);
-        for (int j = 0; j < TAM_TABULEIRO; j++) {
+    for (int i = 0; i < COLUNA; i++) {
+        for (int j = 0; j < LINHA; j++)
             printf("%d ", tabuleiro[i][j]);
-        }
         printf("\n");
     }
 
     return 0;
 }
+
 
